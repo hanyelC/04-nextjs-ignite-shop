deceba942a22aaf50f73d971ad64ebb1da2c218f
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-17 10:17:44 -0300

    feat: configurate font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
 import type { AppProps } from 'next/app'
+import { Roboto_Flex } from 'next/font/google'
+
+const roboto = Roboto_Flex({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router"
+
+export default function Foo() {
+  const { query } = useRouter()
+
+  console.log(query)
+
+  return (
+    <h1>Foo { query.id }</h1>
+  )
+}
