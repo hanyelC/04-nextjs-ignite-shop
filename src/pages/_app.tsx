4a803aa2288c443ce834101b87c160d1d71200a0
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-17 11:02:07 -0300

    feat: add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
+import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 import { Roboto_Flex } from 'next/font/google'
 
 const roboto = Roboto_Flex({ subsets: ['latin'] })
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
