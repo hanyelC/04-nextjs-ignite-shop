34d689c7983b7aefc6a79ade9d5a2055666732ae
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-17 11:19:47 -0300

    feat: add header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 import { Roboto_Flex } from 'next/font/google'
+
+import { globalStyles } from '@/styles/global'
+import logoImg from '@/assets/logo.svg'
+import { Container, Header } from '@/styles/pages/app'
+import Image from 'next/image'
 
 const roboto = Roboto_Flex({ subsets: ['latin'] })
 
@@ -8,8 +12,11 @@ globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={roboto.className}>
+    <Container className={roboto.className}>
+      <Header>
+        <Image {...logoImg} alt="" />
+      </Header>
       <Component {...pageProps} />
-    </main>
+    </Container>
   )
 }
